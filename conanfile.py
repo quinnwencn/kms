d942688f90a6e1a21ccf992ee971e2c05ad58c99
Quinn <quinnwen.cn@gmail.com>
2025-04-14 21:12:57 +0800

    Add python requirements.txt
    Add python virtual env to build with conan
    
    Signed-off-by: Quinn <quinnwen.cn@gmail.com>

--- conanfile.py
+++ conanfile.py
@@ -1,5 +1,5 @@
 from conan import ConanFile
-from conan.tools.cmake import CMake, CMakeToolchain, CMakeDeps
+from conan.tools.cmake import CMake, CMakeToolchain, CMakeDeps, cmake_layout
 
 
 class Kms(ConanFile):
